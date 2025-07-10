890ff436174e9bd472e6de66a8a7d90785854172
Your Name <you@example.com>
2025-07-10 16:11:40 +0500

    fix

--- job/src/App.vue
+++ job/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HeaderTop from './components/HeaderTop.vue'
 import HeaderMiddle from "./components/HeaderMiddle.vue";
+import HeaderEnd from './components/HeaderEnd.vue'
 </script>
 <template>
   <HeaderTop/>
   <HeaderMiddle/>
+  <HeaderEnd/>
  
 </template>
---
+++ job/src/components/HeaderEnd.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="bg-[#F9F9F9] mt-[48px]">
+    <div class="max-w-[1200px] mx-auto">
+      <div class=" py-[16px] flex items-center gap-[50px]">
+        <div class="flex items-center gap-[2px]">
+          <p class="font-semibold text-[15px]">
+            Bakery
+          </p>
+          <svg class="w-4 h-4 ml-1 text-green-600" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.292l3.71-4.06a.75.75 0 111.08 1.04l-4.25 4.655a.75.75 0 01-1.08 0L5.25 8.27a.75.75 0 01-.02-1.06z" clip-rule="evenodd" />
+          </svg>
+        </div>
+        <div class="flex items-center gap-[2px]">
+          <p class="font-semibold text-[15px]">
+            Fruit and vegetables
+          </p>
+          <svg class="w-4 h-4 ml-1 text-green-600" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.292l3.71-4.06a.75.75 0 111.08 1.04l-4.25 4.655a.75.75 0 01-1.08 0L5.25 8.27a.75.75 0 01-.02-1.06z" clip-rule="evenodd" />
+          </svg>
+        </div>
+        <div class="flex items-center gap-[2px]">
+          <p class="font-semibold text-[15px]">
+            Meat and fish
+          </p>
+          <svg class="w-4 h-4 ml-1 text-green-600" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.292l3.71-4.06a.75.75 0 111.08 1.04l-4.25 4.655a.75.75 0 01-1.08 0L5.25 8.27a.75.75 0 01-.02-1.06z" clip-rule="evenodd" />
+          </svg>
+        </div>
+        <div class="flex items-center gap-[2px]">
+          <p class="font-semibold text-[15px]">
+            Drinks
+          </p>
+          <svg class="w-4 h-4 ml-1 text-green-600" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.292l3.71-4.06a.75.75 0 111.08 1.04l-4.25 4.655a.75.75 0 01-1.08 0L5.25 8.27a.75.75 0 01-.02-1.06z" clip-rule="evenodd" />
+          </svg>
+        </div>
+        <div class="flex items-center gap-[2px]">
+          <p class="font-semibold text-[15px]">
+            Kitchen
+          </p>
+          <svg class="w-4 h-4 ml-1 text-green-600" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.292l3.71-4.06a.75.75 0 111.08 1.04l-4.25 4.655a.75.75 0 01-1.08 0L5.25 8.27a.75.75 0 01-.02-1.06z" clip-rule="evenodd" />
+          </svg>
+        </div>
+        <div class="flex items-center gap-[2px]">
+          <p class="font-semibold text-[15px]">
+            Special nutrition
+          </p>
+          <svg class="w-4 h-4 ml-1 text-green-600" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.292l3.71-4.06a.75.75 0 111.08 1.04l-4.25 4.655a.75.75 0 01-1.08 0L5.25 8.27a.75.75 0 01-.02-1.06z" clip-rule="evenodd" />
+          </svg>
+        </div>
+        <div class="flex items-center gap-[2px]">
+          <p class="font-semibold text-[15px]">
+            Baby
+          </p>
+          <svg class="w-4 h-4 ml-1 text-green-600" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.292l3.71-4.06a.75.75 0 111.08 1.04l-4.25 4.655a.75.75 0 01-1.08 0L5.25 8.27a.75.75 0 01-.02-1.06z" clip-rule="evenodd" />
+          </svg>
+        </div>
+        <div class="flex items-center gap-[2px]">
+          <p class="font-semibold text-[15px]">
+            Pharmacy
+          </p>
+          <svg class="w-4 h-4 ml-1 text-green-600" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.292l3.71-4.06a.75.75 0 111.08 1.04l-4.25 4.655a.75.75 0 01-1.08 0L5.25 8.27a.75.75 0 01-.02-1.06z" clip-rule="evenodd" />
+          </svg>
+        </div>
+
+
+      </div>
+    </div>
+  </div>
+</template>
